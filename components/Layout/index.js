627509160213f56c86f4ff6636509bd98e0b43d3
N3tw0rk <khoirrurrohman@gmail.com>
2021-07-07 14:30:09 +0700

    theme color for mobile

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -10,6 +10,7 @@ const Layout = ({ children }) => {
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#E53E3E"></meta>
       </Head>
       {children}
     </Box>
